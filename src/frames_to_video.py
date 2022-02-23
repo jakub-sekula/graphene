7429b67a4221c4140c0662fb33f8d6d5adf779e7
Jakub Sekula <jxs1153@student.bham.ac.uk>
2022-02-23 17:08:51 +0000

    Changed filename of video_to_frames and added some progress screenshots into ./docs

--- src/frames_to_video.py
+++ src/frames_to_video.py
@@ -1,23 +1,34 @@
 import cv2
 import numpy as np
 import glob
+import os
 
-OUTPUT_PATH="C:\\Users\\UVis\\Desktop\\test\\video_slowed2.mp4"
-IMAGES_PATH="C:\\Users\\UVis\\Desktop\\test\\images\\*.jpg"
+OUTPUT_PATH="C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\"
+IMAGES_PATH="C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\images\\*.jpg"
 
 
 def main():
-    frameSize = (640, 480)
+    f = str(input("Enter output filename: "))
 
-    out = cv2.VideoWriter(OUTPUT_PATH,cv2.VideoWriter_fourcc(*'MP4V'), 30, frameSize)
+    frameSize = (1280,1024)
+
+    out = cv2.VideoWriter(os.path.join(OUTPUT_PATH,f+".mp4"),cv2.VideoWriter_fourcc(*'MP4V'), 30, frameSize)
+    count = 0
 
     for filename in glob.glob(IMAGES_PATH):
-        print(filename)
+        if (count % 10 == 0): print(filename)
         img = cv2.imread(filename)
         img_resized = cv2.resize(img, frameSize)
         out.write(img_resized)
+        count += 1
 
     out.release()
+
+    os.chdir(os.path.join(OUTPUT_PATH,"images"))
+
+    for file in os.listdir():
+        os.remove(file)
+
 
 if __name__=='__main__':
     main()
--- src/video_to_frames.py
+++ src/video_to_frames.py
@@ -2,7 +2,7 @@ import cv2
 import sys
 import os
 
-VIDEO_PATH="C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\video.avi"
+VIDEO_PATH="C:\\Users\\UVis\\Desktop\\graphene\\videos\\"
 EXPORT_PATH="C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\images\\"
 
 class avi2jpg:
@@ -16,13 +16,17 @@ class avi2jpg:
         count = 0
         while success:
             framecount = "{number:06}".format(number=count)
-            cv2.imwrite(self.output_path+framecount+".jpg", image) 
+            cv2.imwrite(self.output_path+framecount+".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 100]) 
             success,image = vidcap.read()
-            print('Read a new frame: ', success)
+            if (count % 10 == 0): print('Read a new frame: ', count, success)
             count += 1
 
 if __name__ == "__main__":
-    converter = avi2jpg(VIDEO_PATH, EXPORT_PATH)
+    filename = str(input("Enter filename: "))
+
+    converter = avi2jpg(os.path.join(VIDEO_PATH,filename), EXPORT_PATH)
     converter.convert()
 
+        
+        
         
