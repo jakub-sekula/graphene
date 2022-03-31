4fcdf3e191a00edfb063ca0ca234f249d896605b
Jakub Sekula <seklerek@gmail.com>
2022-03-31 16:26:07 +0100

    Updated notebook files and file structure

--- src/frames_to_video.py
+++ src/frames_to_video.py
@@ -3,20 +3,22 @@ import numpy as np
 import glob
 import os
 
-OUTPUT_PATH="C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\"
-IMAGES_PATH="C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\images\\*.jpg"
+
+OUTPUT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tests', 'Particle tracking', 'Videos', 'Slow motion'))
+IMAGES_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tests', 'Particle tracking', 'Images'))
 
 
 def main():
-    f = str(input("Enter output filename: "))
+    f = str(input("Enter filename of original video: "))
 
-    frameSize = (1280,1024)
+    frameSize = (1280, 1024)
 
-    out = cv2.VideoWriter(os.path.join(OUTPUT_PATH,f+".mp4"),cv2.VideoWriter_fourcc(*'MP4V'), 30, frameSize)
+    out = cv2.VideoWriter(os.path.join(OUTPUT_PATH, f + ".mp4"), cv2.VideoWriter_fourcc(*'MP4V'), 30, frameSize)
     count = 0
 
     for filename in glob.glob(IMAGES_PATH):
-        if (count % 10 == 0): print(filename)
+        if (count % 10 == 0):
+            print(filename)
         img = cv2.imread(filename)
         img_resized = cv2.resize(img, frameSize)
         out.write(img_resized)
@@ -24,11 +26,11 @@ def main():
 
     out.release()
 
-    os.chdir(os.path.join(OUTPUT_PATH,"images"))
+    os.chdir(os.path.join(OUTPUT_PATH, "images"))
 
-    for file in os.listdir():
-        os.remove(file)
+#     for file in os.listdir():
+#         os.remove(file)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
--- src/video_to_frames.py
+++ src/video_to_frames.py
@@ -2,31 +2,42 @@ import cv2
 import sys
 import os
 
-VIDEO_PATH="C:\\Users\\UVis\\Desktop\\graphene\\videos\\"
-EXPORT_PATH="C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\images\\"
+VIDEO_PATH = "C:\\Users\\UVis\\Desktop\\graphene\\videos\\"
+EXPORT_PATH = "C:\\Users\\UVis\\Desktop\\graphene\\tests\\outputs\\images\\"
+
+VIDEO_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tests', 'Particle tracking', 'Videos'))
+EXPORT_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tests', 'Particle tracking', 'Images'))
+
 
 class avi2jpg:
-    def __init__(self,input_path,output_path):
+    def __init__(self, input_path, output_path, original_filename):
         self.input_path = input_path
         self.output_path = output_path
+        self.original_filename = os.path.splitext(original_filename)[0]
+        print(self.original_filename)
 
     def convert(self):
+
+        self.export_folder = os.path.join(self.output_path, self.original_filename)
+
+        if not os.path.exists(self.export_folder):
+            os.makedirs(self.export_folder)
+            print(f"Created new folder {self.export_folder}")
+
         vidcap = cv2.VideoCapture(self.input_path)
-        success,image = vidcap.read()
+        success, image = vidcap.read()
         count = 0
         while success:
             framecount = "{number:06}".format(number=count)
-            cv2.imwrite(self.output_path+framecount+".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 100]) 
-            success,image = vidcap.read()
-            if (count % 10 == 0): print('Read a new frame: ', count, success)
+            cv2.imwrite(os.path.join(self.export_folder, framecount) + ".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+            success, image = vidcap.read()
+            if (count % 10 == 0):
+                print('Read a new frame: ', count, success)
             count += 1
 
+
 if __name__ == "__main__":
-    filename = str(input("Enter filename: "))
+    filename = str(input("Enter filename of input video file (with extension): "))
 
-    converter = avi2jpg(os.path.join(VIDEO_PATH,filename), EXPORT_PATH)
+    converter = avi2jpg(os.path.join(VIDEO_PATH, filename), EXPORT_PATH, filename)
     converter.convert()
-
-        
-        
-        
